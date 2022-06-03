a75f8588fb4d8e851d9e9d564179fd88d29a3a40
Алексей Зосимов <lesha.spb@gmail.com>
2022-06-04 00:54:35 +0300

    Common files

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,16 +1,5 @@
-from .core_actions import BadAction, TradeAction
-from .market_providers.test_market_provider import TestMarketProvider
-from .context import Context, ContextWithDomains
-from .metrics import MetricCollector
-from .observation_builder.basic import ObservationBuilderBasic, ObservationBuilderBasicCache
-from .observation_builder.with_trend_indicator import ObservationBuilderFutureFeature, ObservationBuilderFutureFeatureCache
+from .actions import BadAction, TradeAction
 
-#from .dataset_tools.data_point_factory import DataPointFactoryError, DataShapeError, DataExpiredError
-
-from .core import TrainCoreBasic, TrainCoreFutureFeature
-from .core import TradeCoreBasic, TradeCoreFutureFeature
-from .core import CoreError
-
-
-
-
+from .facade import CoreFacade
+from .facade import TrainCoreBasic, TradeCoreBasic
+from .facade import TrainCoreFutureFeature, TradeCoreFutureFeature
