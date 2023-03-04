eb6aa3515717234c702f6a0fe5e86b01dc7836ec
Алексей Зосимов <lesha.spb@gmail.com>
2023-03-04 19:13:54 +0300

    Actions refactored Link to context deleted

--- src/core2/actions/__init__.py
+++ src/core2/actions/__init__.py
@@ -1,3 +1,3 @@
 from .base_action import BaseAction
 from .bad_action import BadAction
-from .trade_action import TradeAction, OppositeTradeAction, AbstractTradeAction
+from .trade_action import TradeAction
