d1084e385815b42737165ada51bd67a45ae7b861
Алексей Зосимов <lesha.spb@gmail.com>
2023-09-29 20:07:16 +0300

    no message

--- src/train_tools/__init__.py
+++ src/train_tools/__init__.py
@@ -1,6 +1,7 @@
 from .player import Player
 from .live_train_plot import LiveTrainPlot
 from .train_manager.train_manager import TrainManager
+from .train_manager.train_manager_episode import TrainManagerEpisode
 from .train_plot import TrainPlot4
 
 from .plot_and_go import plot_and_go
@@ -8,5 +9,7 @@ from .distribution_analysis import distribution_analysis
 from .get_max_step_factor import get_max_step_factor
 
 #from .train_plot import TrainPlot
+
+from .telegram import TelegramSend
 
 
